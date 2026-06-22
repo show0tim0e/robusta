cdf4c50d20ad1fe754df0ce6e1ce9cb328b6b3cd
Cedrik Urbank <robocrow2018@gmx.de>
2026-06-22 11:56:47 +0200

    Implement scanner logic and test

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -1,1 +1,33 @@
-# TODO
+
+
+from tests.datasets.test_mnist import _ensure_dataset_artifact
+from tests.models.test_resnet18_mnist import _ensure_model_checkpoint
+from vl_scanner.attacks.fgsm import FGSM
+from vl_scanner.core.providers.dataset import DatasetProvider
+from vl_scanner.core.providers.model import ModelProvider
+from vl_scanner.core.scanner import AttackConfig, Scanner
+
+
+def test_resnet18_mnist_scan():
+    scanner = Scanner(
+        dataset=DatasetProvider.load(_ensure_dataset_artifact()),
+        model=ModelProvider.load(_ensure_model_checkpoint()),
+        attacks=[
+            AttackConfig(attack=FGSM, params={"epsilon": 0.03}),
+        ],
+    )
+
+    results = scanner.run()
+
+    for result in results:
+        accuracy = (result.pred == result.y).float().mean()
+        adv_accuracy = (result.adv_pred == result.y).float().mean()
+
+        avg_conf = result.confidence.mean()
+        avg_adv_conf = result.adv_confidence.mean()
+
+        print(f"\nAttack: {result.attack_name}")
+        print(f"Accuracy:      {accuracy:.2%}")
+        print(f"Adv Accuracy:  {adv_accuracy:.2%}")
+        print(f"Confidence:    {avg_conf:.4f}")
+        print(f"Adv Confidence:{avg_adv_conf:.4f}")
--- vl_scanner/core/scanner.py
+++ vl_scanner/core/scanner.py
@@ -1,17 +1,79 @@
 from collections.abc import Sequence
 from dataclasses import dataclass, field
+from typing import Any
 
+import torch
 from torch import Tensor
 from torch.nn import Module
 
 from vl_scanner.attacks.base import Attack
 
 
+@dataclass(slots=True, frozen=True)
+class AttackConfig:
+    attack: type[Attack]
+    params: dict[str, Any] = field(default_factory=dict)
+
+@dataclass(slots=True)
+class AttackResult:
+    attack_name: str
+
+    x: Tensor
+    x_adv: Tensor
+    y: Tensor
+
+    pred: Tensor
+    adv_pred: Tensor
+
+    confidence: Tensor
+    adv_confidence: Tensor
+
 @dataclass(slots=True)
 class Scanner:
     model: Module
     dataset: tuple[Tensor, Tensor]
-    attacks: Sequence[type[Attack]] = field(default_factory=tuple)
+    attacks: Sequence[AttackConfig] = field(default_factory=tuple)
 
-    def run(self) -> None:
-        pass
+    def run(self) -> list[AttackResult]:
+        x, y = self.dataset
+
+        self.model.eval()
+
+        # normal image classification
+        with torch.no_grad():
+            logits = self.model(x)
+            probs = torch.softmax(logits, dim=1)
+
+            confidence, pred = probs.max(dim=1)
+
+        results: list[AttackResult] = []
+
+        # run attacks
+        for attack_config in self.attacks:
+            x_adv = attack_config.attack.generate(
+                self.model,
+                x,
+                y,
+                **attack_config.params
+            )
+
+            with torch.no_grad():
+                adv_logits = self.model(x_adv)
+                adv_probs = torch.softmax(adv_logits, dim=1)
+
+                adv_confidence, adv_pred = adv_probs.max(dim=1)
+
+            result = AttackResult(
+                attack_name=attack_config.attack.name(),
+                x=x,
+                x_adv=x_adv,
+                y=y,
+                pred=pred,
+                adv_pred=adv_pred,
+                confidence=confidence,
+                adv_confidence=adv_confidence
+            )
+
+            results.append(result)
+
+        return results
