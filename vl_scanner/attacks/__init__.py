556567380ab40419283dc2bd353ee770c81b7b84
Tim Metz <t.metz@campus.tu-berlin.de>
2026-06-30 18:07:33 +0200

    Add tuap to list

--- vl_scanner/attacks/__init__.py
+++ vl_scanner/attacks/__init__.py
@@ -1,6 +1,7 @@
 from .base import Attack, AttackParameter
 from .fgsm import FGSM
 from .pgd import PGD
+from .tuap import TUAP
 
-__all__ = ["Attack", "AttackParameter", "FGSM", "PGD"]
+__all__ = ["Attack", "AttackParameter", "FGSM", "PGD", "TUAP"]
 
